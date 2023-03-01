326581df5015ea4967e21a73d146d7c424426336
amirisback <faisalamircs@gmail.com>
2023-03-01 17:54:56 +0700

    update: Add library submodule

---
+++ src/main/java/com/frogobox/simplelibrary/SingleFunc.kt
@@ -0,0 +1,17 @@
+package com.frogobox.simplelibrary
+
+import android.util.Log
+
+/**
+ * Created by Faisal Amir on 01/03/23
+ * https://github.com/amirisback
+ */
+
+
+object SingleFunc {
+
+    fun start() {
+        Log.d("SingleFunc", "start: Simple Library")
+    }
+
+}
